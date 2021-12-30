f1357d60ee13e4a89cad46c6cd8cefa284f0eb35
FunkyFeudalFox <arjan_tammer@yahoo.co.uk>
2021-12-30 17:31:39 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import SaladResult from "./components/SaladResult/SaladResult";
+
+const INITIAL_LEAF_VEGS = [
+  "spinach",
+  "lamb's lettuce",
+  "rocket",
+  "swiss chard",
+  "lollo rosso",
+];
 
 function App() {
+  const [leafVegs, setLeafVegs] = useState(INITIAL_LEAF_VEGS);
+  const [generatedSalad, setGeneratedSalad] = useState("");
+
+  const generateHandler = () => {
+    setGeneratedSalad(() => {
+      return leafVegs[Math.floor(Math.random() * (leafVegs.length + 1))];
+    });
+  };
+
+  const addLeafVegHandler = () => {
+    setLeafVegs("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={generateHandler}>Generate salad</button>
+      <SaladResult result={generatedSalad} />
+      <div>
+        <label>Add leaf vegetable</label>
+        <input></input>
+        <button onClick={addLeafVegHandler}>Add</button>
+      </div>
+      {/* <ToppingsSelector /> */}
+      <div>
+        <logo></logo>
+      </div>
     </div>
   );
 }
---
+++ src/components/SaladResult/SaladResult.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SaladResult = (props) => {
+  return <label>{props.result}</label>;
+};
+
+export default SaladResult;
