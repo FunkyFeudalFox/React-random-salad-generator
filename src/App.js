e7dab186f92de2d4938388969d668894a504b535
FunkyFeudalFox <arjan_tammer@yahoo.co.uk>
2022-01-19 19:33:38 +0100

    added selecting toppings, veg and protein in the generateHandler

--- src/App.js
+++ src/App.js
@@ -12,13 +12,57 @@ const INITIAL_LEAF_VEGS = [
   "lollo rosso",
 ];
 
+const INITIAL_TOPPINGS = [
+  "dryroasted chickpeas with spices",
+  "cashews",
+  "provencal croutons",
+  "rye",
+  "dried mango",
+  "dried berries mix",
+  "peanuts",
+  "wood ear mushrooms",
+  "roasted king oyster mushroom",
+  "roasted mushroom",
+  "roasted potato wedges",
+];
+
+const INITIAL_VEG_FRUITS = [
+  "diced beets",
+  "raspberries",
+  "roasted carrots",
+  "roasted bell pepper",
+  "strawberries",
+  "apple slices",
+  "roasted aubergine",
+];
+
+const INITIAL_PROTEINS = [
+  "bacon",
+  "roasted chicken",
+  "salmon",
+  "cottage cheese",
+  "Feta cheese",
+  "Manchego cheese",
+  "vegan bacon",
+  "spiced lentils",
+  "shrimps",
+  "tempeh",
+];
+
 function App() {
   const [leafVegs, setLeafVegs] = useState(INITIAL_LEAF_VEGS);
+  const [toppings, setToppings] = useState(INITIAL_TOPPINGS);
+  const [vegFruits, setVegFruits] = useState(INITIAL_VEG_FRUITS);
+  const [proteins, setProteins] = useState(INITIAL_PROTEINS);
   const [generatedSalad, setGeneratedSalad] = useState("");
 
   const generateHandler = () => {
     setGeneratedSalad(() => {
-      return leafVegs[Math.floor(Math.random() * (leafVegs.length + 1))];
+      return `Salad of ${
+        leafVegs[Math.floor(Math.random() * leafVegs.length)]
+      } with ${toppings[Math.floor(Math.random() * toppings.length)]}, ${
+        vegFruits[Math.floor(Math.random() * vegFruits.length)]
+      } and ${proteins[Math.floor(Math.random() * proteins.length)]}.`;
     });
   };
 
@@ -26,14 +70,41 @@ function App() {
     setLeafVegs("");
   };
 
+  const addToppingsHandler = () => {
+    setToppings("");
+  };
+
+  const addVegFruitsHandler = () => {
+    setVegFruits("");
+  };
+
+  const addProteinsHandler = () => {
+    setProteins("");
+  };
+
   return (
     <div className="App">
       <button onClick={generateHandler}>Generate salad</button>
       <SaladResult result={generatedSalad} />
       <div>
-        <label>Add leaf vegetable</label>
+        <label>leaf vegetable</label>
         <input></input>
-        <button onClick={addLeafVegHandler}>Add</button>
+        <button onClick={addLeafVegHandler}>Add leaf vegetable</button>
+      </div>
+      <div>
+        <label>topping</label>
+        <input></input>
+        <button onClick={addToppingsHandler}>Add topping</button>
+      </div>
+      <div>
+        <label>veg/fruit</label>
+        <input></input>
+        <button onClick={addVegFruitsHandler}>Add veg/fruit</button>
+      </div>
+      <div>
+        <label>protein</label>
+        <input></input>
+        <button onClick={addProteinsHandler}>Add protein</button>
       </div>
       {/* <ToppingsSelector /> */}
       <div>
